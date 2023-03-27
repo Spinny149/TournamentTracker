bda1fb22e43e1f2160a89a02fde9718c627b7b99
Spinny149 <czechulok@gmail.com>
2023-03-27 15:57:58 +0200

    Add project files.

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one team in a Matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score for this team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the matchup that this team campe from as a winner
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
