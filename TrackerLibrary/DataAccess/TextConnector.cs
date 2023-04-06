0a97bae0d8d9ad68bfe91bd248dd1a8002700bc1
Spinny149 <czechulok@gmail.com>
2023-04-06 12:35:03 +0200

    Saving to file wire up

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,15 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModel.csv";
+
+
         //TODO: Make the CreatePrize method actualy save to the tectFile
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            //Load the file and convert to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            //Find the new Id (max +1)
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id= currentId;
+
+            //Add new record with new ID
+            prizes.Add(model);
+
+            //Save the List<string> to file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
