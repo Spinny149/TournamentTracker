f4e0ada73bfab24a933ddad81d67b12d9460defc
Spinny149 <czechulok@gmail.com>
2023-07-24 13:33:12 +0200

    Setting up main window with some prop

--- TrackerWPFUI/ViewModels/ShellViewModel.cs
+++ TrackerWPFUI/ViewModels/ShellViewModel.cs
@@ -4,10 +4,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary;
+using TrackerLibrary.Models;
 
 namespace TrackerWPFUI.ViewModels
 {
     public class ShellViewModel : Conductor<object>
     {
+        public ShellViewModel()
+        {
+            //Initalize the database connection
+            GlobalConfig.InitializeConnections(TrackerLibrary.DatabaseType.Sql);
+
+            _existingTournaments = new BindableCollection<TournamentModel>(GlobalConfig.Connection.GetTournament_All());
+        }
+
+        public void CreateTournament()
+        {                      
+        }
+
+        private BindableCollection<TournamentModel> _existingTournaments;
+
+        public BindableCollection<TournamentModel> ExisitingTournaments
+        {
+            get { return _existingTournaments; }
+            set { _existingTournaments = value; }
+        }
+
+        private TournamentModel _selectedTournament;
+
+        public TournamentModel SelectedTournament
+        {
+            get { return _selectedTournament; }
+            set 
+            { 
+                _selectedTournament = value;
+                NotifyOfPropertyChange(() => SelectedTournament);
+            }
+        }
+
+
+
     }
 }
