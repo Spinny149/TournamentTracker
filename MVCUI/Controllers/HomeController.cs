c1c981ef9cff1e8f05c68c6b2cb4e990e03c5c69
Spinny149 <czechulok@gmail.com>
2023-09-28 21:06:39 +0200

    ASP .Net MVC project setup

---
+++ MVCUI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace MVCUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<TournamentModel> tournaments = GlobalConfig.Connection.GetTournament_All();
+
+            return View(tournaments);
+        }      
+    }
+}
